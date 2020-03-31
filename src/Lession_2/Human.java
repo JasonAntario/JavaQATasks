8f6a25bb604f8be12fbf2b4ca51a70787cc055f4
JasonAntario <jasonantario@gmail.com>
2020-03-31 21:23:25 +0300

    Add class Human

---
+++ src/Lession_2/Human.java
@@ -0,0 +1,22 @@
+package Lession_2;
+
+public class Human {
+    private int age;
+    private boolean canSpeak;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isCanSpeak() {
+        return canSpeak;
+    }
+
+    public void setCanSpeak(boolean canSpeak) {
+        this.canSpeak = canSpeak;
+    }
+}
