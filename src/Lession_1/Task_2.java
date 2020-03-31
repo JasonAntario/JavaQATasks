5471106d069cd4a69b06bb8e26d6139551a40b5c
JasonAntario <jasonantario@gmail.com>
2020-03-31 18:36:40 +0300

    Add Task_2
    Print all numbers from 0 to 25

---
+++ src/Lession_1/Task_2.java
@@ -0,0 +1,11 @@
+package Lession_1;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.print("All numbers: ");
+        while (i <= 25) {
+            System.out.print(i++ + " ");
+        }
+    }
+}
