8a1d68450a7fa32cf08201a13f9c7356208e952d
JasonAntario <jasonantario@gmail.com>
2020-03-31 21:24:06 +0300

    Add class Person extends Human

---
+++ src/Lession_2/Person.java
@@ -0,0 +1,22 @@
+package Lession_2;
+
+public class Person extends Human {
+    private String name;
+    private String surName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+}
