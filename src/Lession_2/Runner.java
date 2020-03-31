bdead25754885a9895b5f49eb5e1fb0700d8077e
JasonAntario <jasonantario@gmail.com>
2020-03-31 21:26:30 +0300

    Create 2 Constructors (manualEngineer and automateEngineer) and print all parameters

---
+++ src/Lession_2/Runner.java
@@ -0,0 +1,12 @@
+package Lession_2;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        Engineer manualEngineer = new Engineer("Manual Engineer", true, false, "Jason", "Born", 34, true);
+        Engineer automateEngineer = new Engineer("Automate Engineer", true, true, "Gordon", "Freeman", 39, false);
+
+        System.out.println(" " + manualEngineer.getSpecialisation() + "; Execute Tests: " + manualEngineer.isExecuteTests() + "; Invent Code: " + manualEngineer.isInventCode() + "; " + manualEngineer.getName() + " " + manualEngineer.getSurName() + "; " + manualEngineer.getAge() + "; Can speak: " + manualEngineer.isCanSpeak());
+        System.out.println(" " + automateEngineer.getSpecialisation() + "; Execute Tests: " + automateEngineer.isExecuteTests() + "; Invent Code: " + automateEngineer.isInventCode() + "; " + automateEngineer.getName() + " " + automateEngineer.getSurName() + "; " + automateEngineer.getAge() + "; Can speak: " + automateEngineer.isCanSpeak());
+    }
+}
