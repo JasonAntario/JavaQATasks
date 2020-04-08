e26649d73d651738c7a68ea53b4c29de034c13b3
JasonAntario <jasonantario@gmail.com>
2020-04-08 18:24:27 +0300

    Class AutomatedTest was added

---
+++ src/DAY_4/Entity/AutomatedTest.java
@@ -0,0 +1,8 @@
+package DAY_4.Entity;
+
+public class AutomatedTest extends Test {
+    public AutomatedTest(TestLevel testLevel, int instability) {
+        super(testLevel);
+        this.setInstability(instability);
+    }
+}
