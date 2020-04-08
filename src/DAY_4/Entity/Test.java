ec78094b55233ab8b8f8795dfd909694bba03d58
JasonAntario <jasonantario@gmail.com>
2020-04-08 18:26:39 +0300

    Class Test was added

---
+++ src/DAY_4/Entity/Test.java
@@ -0,0 +1,45 @@
+package DAY_4.Entity;
+
+import DAY_4.Worker.AutomationEngineer;
+import DAY_4.Worker.Engineer;
+import DAY_4.Worker.TestEngineer;
+
+import java.util.function.Function;
+
+public abstract class Test implements Function<Engineer, Result> {
+    private int complexity;
+    private int instability;
+
+    public int getComplexity() {
+        return complexity;
+    }
+
+    public void setComplexity(int complexity) {
+        this.complexity = complexity;
+    }
+
+    public int getInstability() {
+        return instability;
+    }
+
+    public void setInstability(int instability) {
+        this.instability = instability;
+    }
+
+    public Test(TestLevel testLevel) {
+        this.complexity = testLevel.COMPLEXITY;
+    }
+
+    @Override
+    public Result apply(Engineer engineer) {
+        int anxiety;
+        if ((this instanceof ManualTest && engineer instanceof AutomationEngineer) || (this instanceof AutomatedTest && engineer instanceof TestEngineer))
+            anxiety = engineer.getAnxiety();
+        else
+            anxiety = 1;
+        if (complexity * instability * anxiety > 30)
+            return Result.FAILED;
+        else
+            return Result.PASSED;
+    }
+}
