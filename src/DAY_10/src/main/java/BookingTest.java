bb726cc69bb04e1a43abf2c5221440bca3eee31e
JasonAntario <jasonantario@gmail.com>
2020-05-07 10:39:53 +0300

    BookingTest was finished

--- src/DAY_10/src/main/java/BookingTest.java
+++ src/DAY_10/src/main/java/BookingTest.java
@@ -6,12 +6,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class BookingTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         String date = null;
-        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "extra/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.booking.com/");
         WebElement element = driver.findElement(By.xpath("//*[@id=\"ss\"]"));
@@ -27,7 +28,6 @@ public class BookingTest {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         String datePlusThreeDays = dateFormat.format(threeDays);
         String datePlusSevenDays = dateFormat.format(sevenDays);
-        System.out.println(datePlusSevenDays + " " + datePlusThreeDays);
 
         element = driver.findElement(By.xpath(String.format("//*[contains(@data-date, \"%s\")]", datePlusThreeDays)));
         element.click();
@@ -39,8 +39,7 @@ public class BookingTest {
 
         element = driver.findElement(By.xpath("//*[contains(@class,\"field-adult\")]//input"));
         String adultCount = element.getAttribute("value");
-        System.out.println(adultCount);
-        element = driver.findElement(By.xpath("//*[contains(@aria-label,\"Increase number of Adults\")]/span"));
+        element = driver.findElement(By.xpath("//*[contains(@aria-describedby, \"adult\")][contains(@class, \"add\")]"));
         for (int i = 0; i < (4 - Integer.parseInt(adultCount)); i++) {
             element.click();
         }
@@ -48,17 +47,27 @@ public class BookingTest {
         element = driver.findElement(By.xpath("//*[contains(@class,\"field-rooms\")]//input"));
         String roomCount = element.getAttribute("value");
         System.out.println(roomCount);
-        element = driver.findElement(By.xpath("//*[contains(@aria-label,\"Increase number of Rooms\")]/span"));
+        element = driver.findElement(By.xpath("//*[contains(@aria-describedby, \"no_rooms_desc\")][contains(@class, \"add\")]"));
         for (int i = 0; i < (2 - Integer.parseInt(roomCount)); i++) {
             element.click();
         }
         element = driver.findElement(By.xpath("//*[contains(@type, \"submit\")]"));
         element.click();
-        element = driver.findElement(By.xpath("//*[@id=\"filter_price\"]//a[5]"));
-        element.click();
+        TimeUnit.SECONDS.sleep(4);
+
         element = driver.findElement(By.xpath("//*[contains(@class, \"sort_price\")]/a"));
         element.click();
+        element = driver.findElement(By.xpath("//*[@id=\"filter_price\"]//a[5]"));
+        element.click();
+        String maxPrice = element.getText();
+        maxPrice = maxPrice.replaceAll("\\D+", "");
+        TimeUnit.SECONDS.sleep(2);
 
+        element = driver.findElement(By.xpath("//*[contains(@class, \"bui-price-display\")]/div[2]/div"));
+        String firstPrice = element.getText();
+        firstPrice = firstPrice.replaceAll("\\D+","");
+        int firstOneDayPrice = Integer.parseInt(firstPrice)/7;
+        System.out.println("Price = "+maxPrice+"+; Lower Price = "+firstOneDayPrice);
 
     }
 }
